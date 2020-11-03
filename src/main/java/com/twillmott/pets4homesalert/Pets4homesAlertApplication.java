0046a73063c0cbed45f218c39786ef3a27447d13
Tom Willmott <tom.willmott@hotmail.com>
2020-11-03 21:55:15 +0000

    Initial commit

---
+++ src/main/java/com/twillmott/pets4homesalert/Pets4homesAlertApplication.java
@@ -0,0 +1,13 @@
+package com.twillmott.pets4homesalert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pets4homesAlertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pets4homesAlertApplication.class, args);
+	}
+
+}
